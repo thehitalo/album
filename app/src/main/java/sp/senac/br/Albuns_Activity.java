4d1f2e089b796b1b32519225f1e39b53500f463e
hitalo.pcarvalho <carvhitalo82@gmail.com>
2019-05-08 12:27:17 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/Albuns_Activity.java
@@ -0,0 +1,15 @@
+package sp.senac.br;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sp.senac.br.album.R;
+
+public class Albuns_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.albuns_activity);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/MainActivity.java
@@ -0,0 +1,15 @@
+package sp.senac.br;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sp.senac.br.album.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
